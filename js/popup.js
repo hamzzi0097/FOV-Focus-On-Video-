a0d3cb1e63a9de8ea387ffdad9d7399609e07a9f
bumbeom <pjbmso365@inha.edu>
2023-01-31 01:19:21 +0900

    feat:answer popup func develop

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+function reply_click(clicked_id){
+    if(clicked_id == "btn1"){
+        document.getElementById(clicked_id).style.color = "#00ff00";
+        setTimeout(function() { alert("정답입니다!"); }, 1000);
+        setTimeout(function() { window.open('','_self').close(); }, 3000);
+    }
+    else{
+        document.getElementById(clicked_id).style.color = "#db0d36";
+        setTimeout(function() { alert("오답입니다.. 정답은 "); }, 1000);
+        setTimeout(function() { window.open('','_self').close(); }, 3000);
+    }
+}
