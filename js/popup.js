8537dbfbca2b21744dbfb760042e566c813b6d63
Jimin0430 <105258203+Jimin0430@users.noreply.github.com>
2023-01-31 06:14:36 +0900

    feat:퀴즈 구현

--- js/popup.js
+++ js/popup.js
@@ -2,7 +2,8 @@ function reply_click(clicked_id){
     if(clicked_id == "btn1"){
         document.getElementById(clicked_id).style.color = "#00ff00";
         setTimeout(function() { alert("정답입니다!"); }, 1000);
-        setTimeout(function() { window.open('','_self').close(); }, 3000);
+        setTimeout(function() {  
+            window.open('','_self').close(); }, 3000);
     }
     else{
         document.getElementById(clicked_id).style.color = "#db0d36";
@@ -10,3 +11,6 @@ function reply_click(clicked_id){
         setTimeout(function() { window.open('','_self').close(); }, 3000);
     }
 }
+//$(".stop").on("click", function(){
+//	$("iframe")[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+//});
